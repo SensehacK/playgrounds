e3a420325bb8d2ac26c34d5d8f7f7bd1814b7c08
Kautilya <kautilyasave@gmail.com>
2018-05-23 18:09:07 +0530

    Project Creation Commit

---
+++ FirstMacCLI/FirstMacCLI/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstMacCLI
+//
+//  Created by Kautilya on 23/05/18.
+//  Copyright © 2018 Sensehack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
