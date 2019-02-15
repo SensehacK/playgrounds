a1bf0532ebf14c52227bc48fdc644fb25b2ec54d
Kautilya <kautilyasave@gmail.com>
2019-02-15 14:51:28 +0530

    Added new page via ionic generate command.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) {
 
+  }
+
+  onGoToUsers() {
+    this.navCtrl.navigateForward('/users');
+  }
 }
