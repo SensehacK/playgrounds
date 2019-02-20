9fd6182758fa360778416758230ce081948f900e
Kautilya <kautilyasave@gmail.com>
2019-02-20 19:16:18 +0530

    Shop page generated

---
+++ src/app/shop/shop.page.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.page.html',
+  styleUrls: ['./shop.page.scss'],
+})
+export class ShopPage {
+
+}
