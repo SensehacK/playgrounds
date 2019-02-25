1836d88a88ac994ebe7215f882486f61db2a8995
Kautilya <kautilyasave@gmail.com>
2019-02-25 13:11:48 +0530

    Add 5 pages to the project.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'library', loadChildren: './library/library.module#LibraryPageModule' },
+  { path: 'quotes', loadChildren: './quotes/quotes.module#QuotesPageModule' },
+  { path: 'quote', loadChildren: './quote/quote.module#QuotePageModule' },
+  { path: 'favorites', loadChildren: './favorites/favorites.module#FavoritesPageModule' },
+  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/quote/quote.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { QuotePage } from './quote.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: QuotePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [QuotePage]
+})
+export class QuotePageModule {}
