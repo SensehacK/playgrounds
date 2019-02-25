f9f5c9ebaad5ef040e312da6ea343bdcb7f2d66d
Kautilya <kautilyasave@gmail.com>
2019-02-25 19:23:53 +0530

    Quotes data passing from file to another component.

--- src/app/library/library.page.ts
+++ src/app/library/library.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from 'src/data/quote.interface';
+import quotes from 'src/data/quotes';
+import { QuotesPage } from '../quotes/quotes.page';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-library',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  quoteCollection: { category: string, quotes: Quote[], icon: string }[];
+  quoteObj: { category: string, quotes: Quote[], icon: string };
+  quotesPage: QuotesPage;
 
   ngOnInit() {
+    this.quoteCollection = quotes;
+
   }
+
+  goToQuotesPage(quoteL: any) {
+    console.log(quoteL);
+    const navigationExt = {
+      queryParams: { 'category': quoteL.category, 'quotesText': quoteL.quotes[0].text, 'quotesArr': quoteL.quotes, 'quotesObj': quoteL, 'quoteLObj2': this.quoteObj }
+    };
+    this.quoteObj = quoteL;
+    console.log('printing selected index object');
+    console.log(quoteL);
+    console.log(quoteL.quotes);
+    this.router.navigate(['quotes'], navigationExt);
+
+
+    // this.router.navigate(['quotes'], quoteL);
+  }
+
+
 
 }
--- src/app/quotes/quotes.page.ts
+++ src/app/quotes/quotes.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from 'src/data/quote.interface';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-quotes',
@@ -6,10 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.page.scss'],
 })
 export class QuotesPage implements OnInit {
+  quotePageObj: { category: string, quotes: Quote[], icon: string };
+  quoteObjCategory: string;
+  quoteObjArr: Quote[];
+  quoteObjIcon: string;
 
-  constructor() { }
+  constructor(private router: Router, private routerP: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log("Hello");
+    // console.log(this.navParams.data);
+    this.routerP.queryParams.subscribe(params => {
+      console.log(params.category);
+      console.log(params.quotesText);
+      console.log('Printing early object');
+      console.log(typeof (params.quotesObj));
+      console.log('Printing whole object');
+      console.log(params.quotesObj.quotes);
+      console.log(typeof (params.quotesArr));
+
+      console.log('Printing early quoteLObj2 object');
+      console.log(typeof (params.quoteLObj2));
+
+      for (const item in params.quotesObj['quotes']) {
+        console.log('in forin loop');
+        if (item.hasOwnProperty('person')) {
+          const element = item['person'];
+          console.log(element);
+
+        }
+      }
+
+      // for (const key in object) {
+      //   if (object.hasOwnProperty(key)) {
+      //     const element = object[key];
+
+      //   }
+      // }
+      for (const item in params.quotesArr) {
+        console.log('Hello WARWTAW');
+        console.log(params.quotesArr);
+        console.log(item);
+        // console.log(item[person]);
+        console.log('Hello Sensehack');
+      }
+      //   * ngfor(items in params.quotesArr) {
+
+      // };
+      // this.quoteObjCategory = params[0].category;
+    });
+
   }
 
 }
