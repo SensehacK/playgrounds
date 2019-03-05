48f8a97f168da74a019d652f3ea75ff3f3e39e65
Kautilya <kautilyasave@gmail.com>
2019-03-05 13:46:46 +0530

    library page auto format.

--- src/app/library/library.page.ts
+++ src/app/library/library.page.ts
@@ -8,19 +8,17 @@ import { DataService } from '../services/data.services';
 @Component({
   selector: 'app-library',
   templateUrl: './library.page.html',
-  styleUrls: ['./library.page.scss'],
+  styleUrls: ['./library.page.scss']
 })
 export class LibraryPage implements OnInit {
+  constructor(private router: Router, private dataService: DataService) {}
 
-  constructor(private router: Router, private dataService: DataService) { }
-
-  quoteCollection: { category: string, quotes: Quote[], icon: string }[];
-  quoteObj: { category: string, quotes: Quote[], icon: string };
+  quoteCollection: { category: string; quotes: Quote[]; icon: string }[];
+  quoteObj: { category: string; quotes: Quote[]; icon: string };
   quotesPage: QuotesPage;
 
   ngOnInit() {
     this.quoteCollection = quotes;
-
   }
 
   goToQuotesPage(quoteL: any) {
@@ -29,21 +27,23 @@ export class LibraryPage implements OnInit {
     console.log('Printing from data service');
     console.log(this.dataService.getQuotePageData());
 
-
     console.log(quoteL);
     const navigationExt = {
-      queryParams: { 'category': quoteL.category, 'quotesText': quoteL.quotes[0].text, 'quotesArr': quoteL.quotes, 'quotesObj': quoteL, 'quoteLObj2': this.quoteObj }
+      queryParams: {
+        category: quoteL.category,
+        quotesText: quoteL.quotes[0].text,
+        quotesArr: quoteL.quotes,
+        quotesObj: quoteL,
+        quoteLObj2: this.quoteObj
+      }
     };
     this.quoteObj = quoteL;
     console.log('printing selected index object');
     console.log(quoteL);
     console.log(quoteL.quotes);
     this.router.navigate(['quotes'], navigationExt);
-
+    console.log('Hei');
 
     // this.router.navigate(['quotes'], quoteL);
   }
-
-
-
 }
