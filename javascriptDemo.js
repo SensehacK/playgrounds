59e3aed08a82c31bcf9c4229abaa7e6f92f1e09c
Kautilya <kautilyasave@gmail.com>
2019-03-11 19:32:54 +0530

    Rx Observables different ways to retrieve.

---
+++ javascriptDemo.js
@@ -0,0 +1,5 @@
+var materialsArr = ["Hydrogen", "Oxygen", "Nitrogen", "Beryllium", "Erbium"];
+
+console.log(materialsArr.map(element => element.length));
+// Output should be : Array of element's length.
+// Usage of => operator from ES 6
