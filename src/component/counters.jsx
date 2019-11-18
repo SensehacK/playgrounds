9bb8b641c4299b57bc6c88d9fa9571d556e363a9
Kautilya <kautilyasave@gmail.com>
2019-11-17 19:47:53 -0500

    Clean Component & Parameter passing + Delete data

--- src/component/counters.jsx
+++ src/component/counters.jsx
@@ -3,20 +3,33 @@ import Counter from "./counter";
 import SingularCounter from "./singularCounter";
 
 class Counters extends Component {
-  state = {};
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 3 },
+      { id: 3, value: 2 },
+      { id: 4, value: 3 }
+    ]
+  };
+
+  handleDelete = counterID => {
+    console.log("Delete func called", counterID);
+    const counters = this.state.counters.filter(c => c.id != counterID);
+    // this.state.counters = counters; We can set state like this, React won't notice the change management like angular
+    this.setState({ counters: counters }); // Simplify setState ({counters}) As both are same, destructive construction object something
+  };
+
   render() {
     return (
-      //   <div>
-      //     <Counter />
-      //     <Counter />
-      //     <Counter />
-      //     <Counter />
-      //   </div>
-
       <div>
-        <SingularCounter />
-        <SingularCounter />
-        <SingularCounter />
+        {this.state.counters.map(counter => (
+          <SingularCounter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+            id={counter.id}
+          />
+        ))}
       </div>
     );
   }
--- src/component/singularCounter.jsx
+++ src/component/singularCounter.jsx
@@ -2,32 +2,35 @@ import React, { Component } from "react";
 
 class SingularCounter extends Component {
   state = {
-    count: 2,
-    imgURL: "https://picsum.photos/250",
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value ? this.props.value : 0 // Setting counter value via props
   };
 
   render() {
-    return (
-      <React.Fragment>
-        <h1>Hello Kautilya</h1>
+    console.log(this.props);
 
-        <h3>Increment Counters and properly set data</h3>
-        <span className="badge badge-primary m-2">{this.state.count} </span>
+    return (
+      <div>
+        <h3>Counter #{this.props.id}</h3>
+        <span className="badge badge-primary m-2">{this.state.value} </span>
         <button
           className="btn btn-secondary btn-sm"
           onClick={this.handleIncrement}
         >
-          Counter+1
+          Counter
         </button>
-      </React.Fragment>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
     );
   }
 
   handleIncrement = () => {
-    console.log("Hi in func increment");
     // Updating the state
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 }
 
