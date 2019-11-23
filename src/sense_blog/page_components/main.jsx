96d137b0328ae6f029b5f279d989caeaee914af8
Kautilya <kautilyasave@gmail.com>
2019-11-23 15:38:04 -0500

    Multi-line string Works!
    I swear if React could just follow universal 3 backticks ``` to have multiline data that would have saved my time just with trial and error.

--- src/sense_blog/page_components/main.jsx
+++ src/sense_blog/page_components/main.jsx
@@ -10,23 +10,55 @@ class Main extends Component {
       {
         id: 1,
         title: "Dummy_Title 1",
-        date: "28/11/2019",
+        date: "28/10/2019",
         summary: "HEDFAFW",
-        body: "HEDFAFW"
+        body: (
+          <div>
+            <p>Dark Mode</p>
+
+            <p>
+              Use apps who support Dark Mode extensively if you&rsquo;re using
+              your phone or laptop at night before bed in poor light conditions.
+            </p>
+
+            <p>
+              AMOLED Screens are more beneficial for Dark Mode apps as they
+              would turn only the pixels which needs to be lighten up &amp;
+              others would be off. &quot;Dark Mode\n&quot; + \n&quot; + Use apps
+              who support Dark Mode extensively if you&rsquo;re using your phone
+              or laptop at night before bed in poor light conditions.\n&quot; +
+              \n&quot; + AMOLED Screens are more beneficial for Dark Mode apps
+              as they would turn only the pixels which needs to be lighten up
+              &amp; others would be off.\n&quot; + &quot;
+            </p>
+          </div>
+        )
       },
       {
         id: 2,
         title: "Dummy_Title 2",
         date: "28/11/2019",
         summary: "HEDFAFW",
-        body: "HEDFAFW"
+        body:
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A doloribus architecto voluptatibus ab iusto magni consequuntur \n" +
+          "fuga quas deserunt aspernatur unde, veritatis non, sapiente officiis officia recusandae hic commodi voluptatum, excepturi odio quod! \n" +
+          "Obcaecati saepe velit cumque sapiente in, nemo tenetur, aspernatur porro beatae dolorem sunt maiores error. Placeat blanditiis excepturi \n" +
+          "alias ex maxime earum eum dolore tempore. Optio qui, sapiente voluptatem laborum quis corporis voluptate error eligendi nihil \n" +
+          "autem officia recusandae odit tenetur totam amet, adipisci, iusto atque quia tempore modi. Sunt, excepturi praesentium vero enim ex\n" +
+          "earum similique quibusdam, consectetur pariatur ut aut nihil? Molestiae inventore eum qui?"
       },
       {
         id: 3,
         title: "Dummy_Title 3",
-        date: "28/11/2019",
+        date: "28/12/2019",
         summary: "HEDFAFW",
-        body: "HEDFAFW"
+        body:
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A doloribus architecto voluptatibus ab iusto magni consequuntur \n" +
+          "fuga quas deserunt aspernatur unde, veritatis non, sapiente officiis officia recusandae hic commodi voluptatum, excepturi odio quod! \n" +
+          "Obcaecati saepe velit cumque sapiente in, nemo tenetur, aspernatur porro beatae dolorem sunt maiores error. Placeat blanditiis excepturi \n" +
+          "alias ex maxime earum eum dolore tempore. Optio qui, sapiente voluptatem laborum quis corporis voluptate error eligendi nihil \n" +
+          "autem officia recusandae odit tenetur totam amet, adipisci, iusto atque quia tempore modi. Sunt, excepturi praesentium vero enim ex\n" +
+          "earum similique quibusdam, consectetur pariatur ut aut nihil? Molestiae inventore eum qui?"
       }
     ]
   };
