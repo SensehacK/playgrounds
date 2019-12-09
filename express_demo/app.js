5430a512664bae7ae82415417e4374580df9c890
Kautilya <kautilyasave@gmail.com>
2019-12-09 15:16:43 -0500

    (feat) Dynamic IDs & Error Handling
    - Dynamic user ID SQL response
    - Little bit of error handling

--- express_demo/app.js
+++ express_demo/app.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/users', (req, res) => {
+app.get('/custom_user', (req, res) => {
     var user1 = { firstName: 'Kautilya', lastName: 'Save' }
 
     const user2 = { firstName: 'Lebron', lastName: 'James' }
@@ -24,27 +24,61 @@ app.get('/users', (req, res) => {
 })
 
 app.get('/user/:id', (req, res) => {
-    console.log('Fetching user ID', req.params.id);
+    const id = req.params.id;
+    console.log('Fetching user ID', id);
     // res.send('Getting the user ID')
 
     const connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        database: 'test'
+        password: 'kautilya',
+        database: 'node_js'
     })
-    const queryString = 'SELECT * FROM test_two'
-    connection.query(queryString, (err, rows, fields) => {
-        console.log('Printing error ', err);
+    const queryString = 'SELECT * FROM users WHERE id = ?'
+    connection.query(queryString, [id], (err, rows, fields) => {
 
-        console.log('Hi fetching users successfully', rows);
-        res.json(rows)
+        if (err) {
+            console.log('Error in the executing query', err);
+            res.send('Something unexpected happened');
+            // res.end()
+        }
+        if (rows.length === 0) {
+            console.log('Empty row query');
+            res.send('Query resulted no results, Please try with different input!')
+            res.end()
+        } else {
+            console.log('Hi fetching users successfully', rows);
+            res.json(rows)
+        }
+
     })
 
     // res.end()
 })
 
+// Getting all the users
+app.get('/users', (req, res) => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'kautilya',
+        database: 'node_js'
+    })
 
-app.listen(3000, () => {
+    const queryString = 'SELECT * FROM users2'
+    connection.query(queryString, (err, rows, field) => {
+        if (err) {
+
+            console.log('Error in the executing query', err);
+            res.send('Something unexpected happened');
+            // res.end()
+        }
+        res.json(rows);
+    })
+})
+
+
+app.listen(3001, () => {
     console.log('Server is up and listening on 3003...');
 })
 
