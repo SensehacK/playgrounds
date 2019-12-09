e5e22843e411402ba490a448294262f3eee31f55
Kautilya <kautilyasave@gmail.com>
2019-12-09 18:13:10 -0500

    More JSON API mapping
    : getConnection func

--- express_demo/app.js
+++ express_demo/app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
 
+
+app.use(express.static('./public'))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('short'))
 
 
@@ -12,6 +16,15 @@ app.get('/', (req, res) => {
     console.log('Hi reached main server');
     res.send('Hello Sensehack!');
 });
+
+const router = express.Router()
+router.get('/messages', (req, res) => {
+    console.log('Show some messages');
+    res.end()
+
+})
+
+app.use(router)
 
 
 app.get('/custom_user', (req, res) => {
@@ -28,12 +41,8 @@ app.get('/user/:id', (req, res) => {
     console.log('Fetching user ID', id);
     // res.send('Getting the user ID')
 
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'kautilya',
-        database: 'node_js'
-    })
+    const connection = getConnection();
+
     const queryString = 'SELECT * FROM users WHERE id = ?'
     connection.query(queryString, [id], (err, rows, fields) => {
 
@@ -48,7 +57,13 @@ app.get('/user/:id', (req, res) => {
             res.end()
         } else {
             console.log('Hi fetching users successfully', rows);
-            res.json(rows)
+            const users = rows.map((row) => {
+                return {
+                    firstName: row.first_name,
+                    lastName: row.last_name
+                }
+            })
+            res.json(users)
         }
 
     })
@@ -58,27 +73,74 @@ app.get('/user/:id', (req, res) => {
 
 // Getting all the users
 app.get('/users', (req, res) => {
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'kautilya',
-        database: 'node_js'
-    })
+    const connection = getConnection()
 
-    const queryString = 'SELECT * FROM users2'
+    const queryString = 'SELECT * FROM users'
     connection.query(queryString, (err, rows, field) => {
-        if (err) {
 
+        if (err) {
             console.log('Error in the executing query', err);
             res.send('Something unexpected happened');
             // res.end()
         }
-        res.json(rows);
+
+        if (rows.length === 0) {
+            console.log('Empty row query');
+            res.send('Query resulted no results, Please try with different input!')
+            res.end()
+        } else {
+            console.log('Hi fetching users successfully', rows);
+            const users = rows.map((row) => {
+                return {
+                    firstName: row.first_name,
+                    lastName: row.last_name
+                }
+            })
+            res.json(users)
+        }
     })
 })
 
+app.post('/user_create', (req, res) => {
+    console.log('In user Create service API endpoint');
+    console.log('Logging the req of post data');
 
-app.listen(3001, () => {
+    const first_name = req.body.first_name
+    const last_name = req.body.last_name
+
+
+    const queryString = 'INSERT INTO  users (first_name, last_name) VALUES (? , ?)'
+
+    getConnection().query(queryString, [first_name, last_name], (err, results, field) => {
+
+        if (err) {
+            console.log('Error in the executing query', err);
+            res.sendStatus(500);
+            return;
+        }
+        console.log('Inserted data to Database', first_name, last_name)
+
+        // We can't use "res object while in the body of executing the query"
+        // res.send('Inserted data to Database', first_name, last_name);
+        res.end()
+    })
+
+
+    res.end()
+
+})
+
+
+app.listen(3003, () => {
     console.log('Server is up and listening on 3003...');
 })
+
+function getConnection() {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'kautilya',
+        database: 'node_js'
+    });
+}
 
