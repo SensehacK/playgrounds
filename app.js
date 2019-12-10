c066defa2b74c07f7c1da5a403e76ed1ed3112e0
Kautilya <kautilyasave@gmail.com>
2019-12-09 19:10:53 -0500

    Changed the whole folder structure
    Thanks to Heroku Auto build

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+// Global variables
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
-const EventEmitter = require('events')
 
-// Trying Emitter and events
+// App use binding
+app.use(express.static('./public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('short'))
 
-const Logger = require('./logger');
-const logger = new Logger();
 
-// Listening an event
-logger.on('Kautilya', (args) => {
-    console.log('Event Listened', args);
+// Root access
+app.get('/', (req, res) => {
+    console.log('Hi reached main server');
+    res.send('Hello Sensehack!');
+});
+
+// Router 
+const router = require('./routes/user.js')
+app.use(router)
+
+// Wake up init
+app.listen(3003, () => {
+    console.log('Server is up and listening on 3003...');
 })
-logger.log('SensehacK');
+
+
+
