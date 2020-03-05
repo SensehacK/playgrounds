6fda31b93230a89e94594a65a280792c01d6779e
Kautilya <kautilyasave@gmail.com>
2020-03-04 22:40:51 -0500

    (LeetCode)TwoSum: First success
    After like 3rd attempt my logic was finally clear.

--- swift/LeetCode/Easy/TwoSum.playground/Contents.swift
+++ swift/LeetCode/Easy/TwoSum.playground/Contents.swift
@@ -1,25 +1,44 @@
 import UIKit
 
-var numbers = [2, 7, 11, 15, 14, 5]
-
+var numbers = [3,2,4]
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-//    var diffArr: [Int] = []
-//    for num in nums {
-//        let diff = target - num
-//        diffArr.append(diff)
-//    }
-    
+
     for (index,var value) in nums.enumerated() {
         value = target - value
-        for (index2, var value2) in nums.enumerated() {
-            value2 = target - value2
-            if (target == (value2+value)) {
-                return [index,index2]
+        for i in index+1...nums.count-1 {
+            let value2 = target - nums[i]
+            if (target == (value + value2)) {
+                return [index,i]
             }
         }
     }
     return []
 }
 
-print(twoSum(numbers, 9))
+print(twoSum(numbers, 6))
+
+
+/*
+ 
+ Question :
+ 
+ Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ Example:
+
+ Given nums = [2, 7, 11, 15], target = 9,
+
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
+
+
+ Link: https://leetcode.com/problems/two-sum/
+ 
+ Stats
+Runtime: 468 ms, faster than 26.55% of Swift online submissions for Two Sum.
+Memory Usage: 21 MB, less than 5.88% of Swift online submissions for Two Sum.
+
+ */
