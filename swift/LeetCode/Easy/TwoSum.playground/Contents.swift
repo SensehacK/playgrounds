3cdd588ed22356526c0e3f81e50e9f0a39011672
Kautilya <kautilyasave@gmail.com>
2020-03-04 22:11:14 -0500

    (LeetCode)TwoSum: Easy

--- swift/LeetCode/Easy/TwoSum.playground/Contents.swift
+++ swift/LeetCode/Easy/TwoSum.playground/Contents.swift
@@ -1,40 +1,25 @@
 import UIKit
 
-var str = "Hello, playground"
 var numbers = [2, 7, 11, 15, 14, 5]
-var numbers2 = [2, 11, 15, 14, 5, 9]
 
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+//    var diffArr: [Int] = []
+//    for num in nums {
+//        let diff = target - num
+//        diffArr.append(diff)
+//    }
     
-    var diffArr: [Int] = []
-    for num in nums {
-        let diff = target - num
-        print("Printing diff",diff)
-        if (diff>0)  {
-            diffArr.append(diff)
-        }
-        
-    }
-    
-    for value in diffArr {
-        
-        print("Values of value in diffArr", value)
-    }
-    let firstNumber = diffArr[0]
-    for (index,value) in diffArr.enumerated() {
-        print("Values of diffArr", value)
-        for (index2, value2) in diffArr.enumerated() {
-            print("Adding two values", value2, value, target )
-            print("Addition two values", (value2+value), target )
+    for (index,var value) in nums.enumerated() {
+        value = target - value
+        for (index2, var value2) in nums.enumerated() {
+            value2 = target - value2
             if (target == (value2+value)) {
                 return [index,index2]
             }
         }
     }
-    
     return []
 }
 
-//print(twoSum(numbers, 9))
-print(twoSum(numbers2, 11))
+print(twoSum(numbers, 9))
