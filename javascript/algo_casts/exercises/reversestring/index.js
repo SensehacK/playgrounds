d432b3eae2a217905091255a27d48a261c34601a
Kautilya <kautilyasave@gmail.com>
2020-09-05 13:02:15 -0400

    (practise)Algorithm: reverse string

--- javascript/algo_casts/exercises/reversestring/index.js
+++ javascript/algo_casts/exercises/reversestring/index.js
@@ -7,6 +7,17 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
+	var str2 = new String();
+	//var str2; // this leads to having undefined variable as it is not initialized with new String()
+	console.log("printing length" + str.length);
+
+	for (var a = str.length - 1; a >= 0; a--) {
+		console.log("Value of a is " + a);
+
+		console.log(str[a]);
+		str2 += str[a];
+	}
+
 	return str2;
 }
 
