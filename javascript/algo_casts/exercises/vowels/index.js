713f8228ed634ffee8742708c755e61fee0af144
Kautilya <kautilyasave@gmail.com>
2020-09-13 12:01:07 -0400

    (practise)Algorithm: vowels

--- javascript/algo_casts/exercises/vowels/index.js
+++ javascript/algo_casts/exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+	let vowelsArr = ["a", "e", "i", "o", "u"];
+	vowelCount = 0;
+	for (const char of str.toLowerCase()) {
+		if (vowelsArr.includes(char)) {
+			vowelCount++;
+		}
+	}
+	return vowelCount;
+}
 
 module.exports = vowels;
