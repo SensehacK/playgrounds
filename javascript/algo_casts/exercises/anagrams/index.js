73d0cd1a205ad14b7ab463add52420c9e024d230
Kautilya <kautilyasave@gmail.com>
2020-09-14 00:03:34 -0400

    (practise)Algorithm: anagram

--- javascript/algo_casts/exercises/anagrams/index.js
+++ javascript/algo_casts/exercises/anagrams/index.js
@@ -8,6 +8,52 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+	const cleanedStrA = stringA.replace(/[^\w]/g, "");
+	const cleanedStrB = stringB.replace(/[^\w]/g, "");
+	const charsA = {};
+	const charsB = {};
+	var isAnagram = true;
+
+	// Character mapping
+
+	for (let char of cleanedStrA) {
+		if (charsA[char]) {
+			charsA[char] += 1;
+		} else {
+			charsA[char] = 1;
+		}
+	}
+
+	for (let char of cleanedStrB) {
+		if (charsB[char]) {
+			charsB[char] += 1;
+		} else {
+			charsB[char] = 1;
+		}
+	}
+
+	// debug checks
+	console.log(Object.keys(charsA).length);
+	console.log(Object.keys(charsB).length);
+	charsB.hasOwnProperty("f");
+
+	// iterating & comparing key values with each other.
+	// switching out boolean value !true false
+	if (Object.keys(charsA).length == Object.keys(charsB).length) {
+		for (const key in charsA) {
+			if (charsB.hasOwnProperty(key) && charsA.hasOwnProperty(key)) {
+				isAnagram = charsA[key] === charsB[key] ? true : false;
+			} else {
+				isAnagram = false;
+			}
+		}
+	} else {
+		isAnagram = false;
+	}
+	return isAnagram;
+}
+
+anagrams("rail safety", "fairy tales");
 
 module.exports = anagrams;
