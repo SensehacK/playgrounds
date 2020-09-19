c0e2d006b8b2e3cf341b4d6cc679b1bdf0284743
Kautilya <kautilyasave@gmail.com>
2020-09-19 14:52:51 -0400

    (practise)Algorithm: array chunk

--- javascript/algo_casts/exercises/chunk/index.js
+++ javascript/algo_casts/exercises/chunk/index.js
@@ -8,6 +8,69 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+	var arr = [];
+	var subArr = [];
+	var increment = 0;
+	console.log("Starting ########### " + size);
+
+	for (let index = 0; index < array.length; index++) {
+		if (increment < size) {
+			subArr.push(array[index]);
+			increment++;
+		} else {
+			arr.push(subArr);
+			subArr = [];
+			subArr.push(array[index]);
+			increment = 1;
+		}
+		// Endgame if only 1 case last check
+		if (array.length - 1 === index) {
+			arr.push(subArr);
+		}
+	}
+
+	/*
+	array.forEach((element) => {
+		console.log("Printing element: " + element);
+		if (increment < size) {
+			subArr.push(element);
+			increment++;
+		} else {
+			arr.push(subArr);
+			subArr = [];
+			subArr.push(element);
+			increment = 1;
+		}
+	});
+	*/
+
+	/*
+	for (const index of array) {
+		console.log("Printing index: " + index);
+		console.log("Printing element: " + array[index]);
+		console.log("Length of array: " + array.length);
+		if (increment < size) {
+			subArr.push(element);
+			increment++;
+			if (array.length - 1 === index + 1) {
+				arr.push(subArr);
+			}
+		} else {
+			arr.push(subArr);
+			subArr = [];
+			subArr.push(element);
+			increment = 1;
+		}
+	}
+
+	*/
+
+	return arr;
+}
+
+// chunk([1, 2, 3, 4], 2);
+
+chunk([1, 2, 3], 1);
 
 module.exports = chunk;
