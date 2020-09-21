2108871d4783449edd6b25b90c45a3ba6436cb80
Kautilya <kautilyasave@gmail.com>
2020-09-21 11:05:55 -0400

    (practise)Algorithm: reverse int

--- javascript/algo_casts/exercises/reverseint/index.js
+++ javascript/algo_casts/exercises/reverseint/index.js
@@ -8,6 +8,32 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+	console.log("Values of data" + n);
+	console.log(n % 10);
+	const reverseStr = n.toString().split("").reverse().join("");
+	console.log(reverseStr);
+
+	const integer = parseInt(reverseStr);
+	console.log("the value of reverse integer" + integer);
+	console.log("Math.sign: " + Math.sign(n));
+	const tempVar = Math.sign(n) == 1 ? integer : integer - integer * 2;
+	console.log("Variable of temporary" + tempVar);
+
+	// console.log(
+	// 	"Values of data" + Math.sign(n) ? integer : integer - integer * 2
+	// );
+
+	// n.toString();
+	return tempVar;
+}
+
+// reverseInt(500);
+
+// reverseInt(056);
+
+// reverseInt(325);
+
+// reverseInt(-3265);
 
 module.exports = reverseInt;
