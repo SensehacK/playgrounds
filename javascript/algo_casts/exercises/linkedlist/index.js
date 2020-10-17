844b73d7cd4f9f12b235706298c3d3c500e870a3
Kautilya <kautilyasave@gmail.com>
2020-10-16 22:56:53 -0400

    (practise)Algorithm: LinkedList

--- javascript/algo_casts/exercises/linkedlist/index.js
+++ javascript/algo_casts/exercises/linkedlist/index.js
@@ -2,8 +2,23 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
+}
 
-class LinkedList {}
+class LinkedList {
+	constructor() {
+		this.head = null;
+	}
+
+	insertFirst(data) {
+		var tempNode = new Node(data);
+		tempNode.next = this.head;
+		this.head = tempNode;
+	}
+}
 
 module.exports = { Node, LinkedList };
