b520ee76fc23a865bc57eb81870ab44dd29312c7
Kautilya <kautilyasave@gmail.com>
2020-10-17 15:36:19 -0400

    (practise)Algorithm: LinkedList v2

--- javascript/algo_casts/exercises/linkedlist/index.js
+++ javascript/algo_casts/exercises/linkedlist/index.js
@@ -19,6 +19,45 @@ class LinkedList {
 		tempNode.next = this.head;
 		this.head = tempNode;
 	}
+
+	size() {
+		var size = 0;
+		var currentNode = this.head;
+		console.log("#########", currentNode);
+		// console.log("#########", this.head.next);
+
+		// console.log("#### currentNode.next ", currentNode.next);
+		// console.log("#### currentNodetype of ", typeof currentNode.next);
+		// if currentNode
+		while (currentNode) {
+			currentNode = currentNode.next;
+			size++;
+			console.log("Hello next node", currentNode);
+		}
+
+		/*
+		while (currentNode.next != null) {
+			size += 1;
+			currentNode = this.head.next;
+		}
+		*/
+		return size;
+	}
+
+	getFirst() {
+		return this.head;
+	}
 }
+
+// const list = new LinkedList();
+// list.insertFirst(6);
+// list.insertFirst(12);
+// list.insertFirst(9);
+// list.insertFirst(3);
+// console.log("listets");
+// console.log(list);
+
+// const sie = list.size();
+// console.log("Size", sie);
 
 module.exports = { Node, LinkedList };
