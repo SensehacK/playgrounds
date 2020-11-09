8a0731e26c44b3223c0bb6002d4649a7842a5fee
Kautilya <kautilyasave@gmail.com>
2020-11-08 21:09:27 -0500

    (practise)Algorithm: LinkedList Circular

--- javascript/algo_casts/exercises/circular/index.js
+++ javascript/algo_casts/exercises/circular/index.js
@@ -12,6 +12,34 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+	var shortJump = list.getFirst();
+	var fastJump = list.getFirst();
+
+	while (fastJump.next && fastJump.next.next) {
+		// console.log(" while fastJump.next  :", fastJump.next);
+		shortJump = shortJump.next;
+		fastJump = fastJump.next.next;
+		// console.log("next linkedList shortJump : ", shortJump);
+		// console.log("next linkedList fastJump : ", fastJump);
+		if (fastJump === shortJump) {
+			return true;
+		}
+	}
+	return false;
+}
 
 module.exports = circular;
+
+// const L = require("./linkedlist");
+// const Node = L.Node;
+// const LinkedList = L.LinkedList;
+// const l = new LinkedList();
+// const a = new Node("a");
+// const b = new Node("b");
+// const c = new Node("c");
+// l.head = a;
+// a.next = b;
+// b.next = c;
+// c.next = null;
+// console.log(circular(l)); // true
