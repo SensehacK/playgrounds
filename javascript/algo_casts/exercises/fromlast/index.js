349be1d7aacc0bb892dd898b1ee6034adc8cfeb9
Kautilya <kautilyasave@gmail.com>
2020-11-09 22:39:13 -0500

    (practise)Algorithm: LinkedList Last

--- javascript/algo_casts/exercises/fromlast/index.js
+++ javascript/algo_casts/exercises/fromlast/index.js
@@ -11,6 +11,32 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+	var shortJump = list.getFirst();
+	var fastJump = list.getFirst();
+
+	for (let index = 0; index < n; index++) {
+		fastJump = fastJump.next;
+	}
+
+	while (fastJump.next) {
+		shortJump = shortJump.next;
+		fastJump = fastJump.next;
+		console.log("next linkedList shortJump : ", shortJump);
+		console.log("next linkedList fastJump : ", fastJump);
+	}
+
+	return shortJump;
+}
 
 module.exports = fromLast;
+
+const L = require("./linkedlist");
+const Node = L.Node;
+const LinkedList = L.LinkedList;
+const list = new LinkedList();
+list.insertLast("a");
+list.insertLast("b");
+list.insertLast("c");
+list.insertLast("d");
+console.log(fromLast(list, 2).data); // 'b'
