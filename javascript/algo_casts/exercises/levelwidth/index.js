ddee28b82dcbeb155aee539e4ee2552c74aa46d6
Kautilya <kautilyasave@gmail.com>
2020-11-10 21:10:19 -0500

    (practise)Algorithm: LinkedList level width

--- javascript/algo_casts/exercises/levelwidth/index.js
+++ javascript/algo_casts/exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+	var counters = [0];
+	var treeArr = [root, "s"];
+	while (treeArr.length > 1) {
+		const node = treeArr.shift();
+
+		if (node === "s") {
+			counters.push(0);
+			treeArr.push("s");
+		} else {
+			counters[counters.length - 1]++;
+			treeArr.push(...node.children);
+		}
+	}
+	return counters;
+}
 
 module.exports = levelWidth;
+
