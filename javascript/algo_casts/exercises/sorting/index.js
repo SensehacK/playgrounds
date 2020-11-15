fa291e8c7386de85f142931b32dfd6b6c62e837d
Kautilya <kautilyasave@gmail.com>
2020-11-15 18:11:58 -0500

    (practise)Algorithm: Merge Sort & Selection Sort

--- javascript/algo_casts/exercises/sorting/index.js
+++ javascript/algo_casts/exercises/sorting/index.js
@@ -14,10 +14,48 @@ function bubbleSort(arr) {
 	return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+	var indexOfMin = 0;
+	for (let i = 0; i < arr.length; i++) {
+		indexOfMin = i;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[indexOfMin]) {
+				indexOfMin = j;
+			}
+		}
+		if (arr[i] != arr[indexOfMin]) {
+			let swapVariable = arr[i];
+			arr[i] = arr[indexOfMin];
+			arr[indexOfMin] = swapVariable;
+		}
+	}
+	return arr;
+}
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+	if (arr.length === 1) {
+		return arr;
+	}
 
-function merge(left, right) {}
+	const center = Math.floor(arr.length / 2);
+	const left = arr.slice(0, center);
+	const right = arr.slice(center);
+
+	return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+	const results = [];
+
+	while (left.length && right.length) {
+		if (left[0] < right[0]) {
+			results.push(left.shift());
+		} else {
+			results.push(right.shift());
+		}
+	}
+
+	return [...results, ...left, ...right];
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
